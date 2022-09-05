3ac1bf2fdd6fc950dc4c9d7be30d034dfff2cb20
jhj603 <82034869+jhj603@users.noreply.github.com>
2022-09-05 09:42:50 +0900

    Terrain Rect 넣어야함

--- Engine/Public/CollisionMgr.h
+++ Engine/Public/CollisionMgr.h
@@ -1,3 +1,23 @@
+#pragma once
+#include  "Layer.h"
+
+BEGIN(Engine)
+
+class ENGINE_DLL CCollisionMgr
+{
+public:
+	CCollisionMgr();
+	~CCollisionMgr();
+
+public:
+	//리스트끼리 충돌 검사
+	static bool	Collision_Sphere(CLayer::GAMEOBJECTS _Dest, CLayer::GAMEOBJECTS _Sour, CGameObject** pDest, CGameObject** pSour);
+	//개별 충돌 검사
+	static bool Check_Sphere(CGameObject* pDest, CGameObject* pSour);
+};
+
+END
+
 //#pragma once
 //
 //#include "Component.h"
