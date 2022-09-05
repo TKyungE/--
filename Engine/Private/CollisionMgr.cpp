5ece77ad4496673ee976fdb50b93e3f2e1be4919
jhj603 <82034869+jhj603@users.noreply.github.com>
2022-09-05 09:38:18 +0900

    picking file Collider fixing

--- Engine/Private/CollisionMgr.cpp
+++ Engine/Private/CollisionMgr.cpp
@@ -1,28 +1,62 @@
 #include "..\Public\CollisionMgr.h"
 
 
-//bool CCollisionMgr::Collision_Sphere(CLayer::GAMEOBJECTS _Dest, CLayer::GAMEOBJECTS _Sour, CGameObject** pDest, CGameObject** pSour)
+bool CCollisionMgr::Collision_Sphere(CLayer::GAMEOBJECTS _Dest, CLayer::GAMEOBJECTS _Sour, CGameObject** pDest, CGameObject** pSour)
+{
+	for (auto& Dest : _Dest)
+	{
+		for (auto& Sour : _Sour)
+		{
+			if (Check_Sphere(Dest, Sour))
+			{
+				*pDest = Dest;
+				*pSour = Sour;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool CCollisionMgr::Check_Sphere(CGameObject * pDest, CGameObject * pSour)
+{
+	_float fDist = D3DXVec3Length(&(*(_float3*)&pDest->Get_World().m[3][0] - *(_float3*)&pSour->Get_World().m[3][0]));
+	_float	fRadius = pDest->Get_Info().fX + pSour->Get_Info().fX;
+
+
+	return fRadius >= fDist;	// 충돌을 한 경우
+}
+
+//HRESULT CCollisionMgr::Initialize_Prototype(void)
 //{
-//	for (auto& Dest : _Dest)
-//	{
-//		for (auto& Sour : _Sour)
-//		{
-//			if (Check_Sphere(Dest, Sour))
-//			{
-//				*pDest = Dest;
-//				*pSour = Sour;
-//				return true;
-//			}
-//		}
-//	}
-//	return false;
+//	return E_NOTIMPL;
 //}
 //
-//bool CCollisionMgr::Check_Sphere(CGameObject * pDest, CGameObject * pSour)
+//HRESULT CCollisionMgr::Initialize(void * pArg)
 //{
-//	_float fDist = D3DXVec3Length(&(*(_float3*)&pDest->Get_World().m[3][0] - *(_float3*)&pSour->Get_World().m[3][0]));
-//	_float	fRadius = pDest->Get_Info().fX + pSour->Get_Info().fX;
+//	return E_NOTIMPL;
+//}
 //
+//HRESULT CCollisionMgr::Add_ColiisionGroup(COLLISIONGROUP eCollisionGroup, CGameObject * pGameObject)
+//{
+//	return E_NOTIMPL;
+//}
 //
-//	return fRadius >= fDist;	// 충돌을 한 경우
+//HRESULT CCollisionMgr::Collision_AABB(GAMEOBJECTS _Dest, GAMEOBJECTS _Sour)
+//{
+//	return E_NOTIMPL;
+//}
+//
+//CCollisionMgr * CCollisionMgr::Create(LPDIRECT3DDEVICE9 pGraphic_Device)
+//{
+//	return nullptr;
+//}
+//
+//CComponent * CCollisionMgr::Clone(void * pArg)
+//{
+//	return nullptr;
+//}
+//
+//void CCollisionMgr::Free(void)
+//{
 //}
--- Engine/Public/CollisionMgr.h
+++ Engine/Public/CollisionMgr.h
@@ -1,37 +1,37 @@
-#pragma once
-
-#include "Component.h"
-
-BEGIN(Engine)
-
-class ENGINE_DLL CCollisionMgr final : public CComponent
-{
-public:
-	enum COLLISIONGROUP { COLLISION_PLAYER, COLLISION_MONSTER, COLLISION_BOSS, COLLISION_PLAYERSKILL, COLLISION_MONSTERSKILL, COLLISION_OBJECT, COLLISION_END };
-
-private:
-	CCollisionMgr(LPDIRECT3DDEVICE9 pGraphic_Device);
-	virtual ~CCollisionMgr() = default;
-
-public:
-	virtual HRESULT Initialize_Prototype(void) override;
-	virtual HRESULT Initialize(void* pArg) override;
-
-public:
-	HRESULT Add_ColiisionGroup(COLLISIONGROUP eCollisionGroup, class CGameObject* pGameObject);
-
-
-private:
-	list <class CGameObject*> m_GameObjects[COLLISION_END];
-	typedef list<class CGameObject*> GAMEOBJECTS;
-
-private:
-	HRESULT Collision_AABB(GAMEOBJECTS _Dest, GAMEOBJECTS _Sour);
-
-public:
-	static CCollisionMgr* Create(LPDIRECT3DDEVICE9 pGraphic_Device);
-	virtual CComponent* Clone(void* pArg = nullptr) override;
-	virtual void Free(void) override;
-};
-
-END
+//#pragma once
+//
+//#include "Component.h"
+//
+//BEGIN(Engine)
+//
+//class ENGINE_DLL CCollisionMgr final : public CComponent
+//{
+//public:
+//	enum COLLISIONGROUP { COLLISION_PLAYER, COLLISION_MONSTER, COLLISION_BOSS, COLLISION_PLAYERSKILL, COLLISION_MONSTERSKILL, COLLISION_OBJECT, COLLISION_END };
+//
+//private:
+//	CCollisionMgr(LPDIRECT3DDEVICE9 pGraphic_Device);
+//	virtual ~CCollisionMgr() = default;
+//
+//public:
+//	virtual HRESULT Initialize_Prototype(void) override;
+//	virtual HRESULT Initialize(void* pArg) override;
+//
+//public:
+//	HRESULT Add_ColiisionGroup(COLLISIONGROUP eCollisionGroup, class CGameObject* pGameObject);
+//
+//
+//private:
+//	list <class CGameObject*> m_GameObjects[COLLISION_END];
+//	typedef list<class CGameObject*> GAMEOBJECTS;
+//
+//private:
+//	HRESULT Collision_AABB(GAMEOBJECTS _Dest, GAMEOBJECTS _Sour);
+//
+//public:
+//	static CCollisionMgr* Create(LPDIRECT3DDEVICE9 pGraphic_Device);
+//	virtual CComponent* Clone(void* pArg = nullptr) override;
+//	virtual void Free(void) override;
+//};
+//
+//END
